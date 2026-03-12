795e8f03d1636d8d5078de2ad44c7a787f28d460
Rajneesh Kushwaha <160695415+Rajneesh-1234@users.noreply.github.com>
2026-03-12 18:59:36 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "Ezeeblitz-React-App",
+  base: "/Ezeeblitz-React-App/",
   plugins: [react()],
 })
